d77a968b592ff797df744a1adb082631c8332eed
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2024-03-19 21:50:28 -0500

    All selectors active classes work. Until Clicked each selector will not have active in their className. Once clicked the selector will have an active className. If clicked while active active className is removed. If another selector is clicked active class will be removed from 	the selector and the new selector will have active class added.

--- src/Functional/FunctionalSection.tsx
+++ src/Functional/FunctionalSection.tsx
@@ -4,8 +4,12 @@ import { Link } from "react-router-dom";
 import { FunctionalDogs } from "./FunctionalDogs";
 import { FunctionalCreateDogForm } from "./FunctionalCreateDogForm";
 import { SectionSelector } from "../Shared/Selectors";
+import { useState } from "react";
 
 export const FunctionalSection = () => {
+	const [isFavActive, setIsFavActive] = useState("");
+	const [isUnFavActive, setIsUnFavActive] = useState("");
+	const [isCreateDogActive, setIsCreateDogActive] = useState("");
 	return (
 		<section id="main-section">
 			<div className="container-header">
@@ -20,18 +24,44 @@ export const FunctionalSection = () => {
 					{/* This should display the favorited count */}
 					<SectionSelector
 						section="favorited"
-						activeClass=""
+						activeClass={isFavActive}
 						count={12}
+						onClick={() => {
+							setIsFavActive("active");
+							if (
+								isUnFavActive === "active" ||
+								isCreateDogActive === "active"
+							) {
+								setIsCreateDogActive("");
+								setIsUnFavActive("");
+							}
+							if (isFavActive === "active") setIsFavActive("");
+						}}
 					/>
 					{/* This should display the unfavorited count */}
 					<SectionSelector
 						section="unfavorited"
-						activeClass=""
+						activeClass={isUnFavActive}
 						count={25}
+						onClick={() => {
+							setIsUnFavActive("active");
+							if (isFavActive === "active" || isCreateDogActive === "active") {
+								setIsCreateDogActive("");
+								setIsFavActive("");
+							}
+							if (isUnFavActive === "active") setIsUnFavActive("");
+						}}
 					/>
 					<div
-						className={`selector`}
-						onClick={() => {}}
+						className={`selector ${isCreateDogActive}`}
+						onClick={() => {
+							setIsCreateDogActive("active");
+							if (isUnFavActive === "active" || isFavActive === "active") {
+								setIsFavActive("");
+								setIsUnFavActive("");
+							}
+							if (isCreateDogActive === "active") setIsCreateDogActive("");
+						}}
 					>
 						create dog
 					</div>
--- src/Shared/Selectors.tsx
+++ src/Shared/Selectors.tsx
@@ -2,16 +2,18 @@ export const SectionSelector = ({
 	section,
 	count,
 	activeClass,
+	onClick,
 }: {
 	section: string;
 	count: number;
 	activeClass: string | null;
+	onClick: (event: React.MouseEvent<HTMLElement>) => void;
 }) => {
 	return (
 		<>
 			<div
 				className={`selector ${activeClass}`}
-				onClick={() => {}}
+				onClick={onClick}
 			>
 				{section} ({count})
 			</div>
